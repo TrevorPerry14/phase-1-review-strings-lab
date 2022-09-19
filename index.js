95a08bddf4ee72fe8e3d7a3aca1e5ea09e246c18
Trevor <trevorperry143@gmail.com>
2022-09-19 12:03:04 -0500

    commit tree dc5950b1b4e4631104d8c4d360659e92b4745bf0

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Trevor Perry'
+let welcomeMessage = "Welcome to Flatbook, Trevor Perry!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, TREVOR PERRY!"
+let shortGreeting = "Welcome, T!"
